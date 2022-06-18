80931ac71961fcdba48332b6f3ca445e62bfc7bb
Shubham <shubhamvishwakarma0604@gmail.com>
2022-06-18 05:52:37 +0530

    Legacy Files Added

---
+++ index.js
@@ -0,0 +1,54 @@
+const Seconds = document.querySelector(".seconds")
+const Minute = document.querySelector(".minute")
+const Hour = document.querySelector(".hour")
+const Start = document.querySelector("#start")
+const Pause = document.querySelector("#pause")
+const Reset = document.querySelector("#reset")
+let timer_state = true;
+let sec = 0;
+let min = 0;
+let hr = 0;
+
+Reset.addEventListener("click", ()=> {
+	Seconds.innerText = Minute.innerText = Hour.innerText  = "00";
+	sec = min = hr = 0;
+	timer_state = false;
+
+})
+
+Start.addEventListener("click", () => {
+	timer_state = true;
+	timer();
+})
+
+Pause.addEventListener("click", ()=>{
+	timer_state = false;
+})
+
+
+
+function timer() {
+	if (timer_state) {
+		Seconds.innerText = sec;
+		sec++;
+
+		if (min == 60) {
+			hr++;
+			Hour.innerText = hr;
+			sec = 0
+			min = 0;
+		}
+
+		if (sec==60) {
+			min++
+			Minute.innerText = min;
+			sec = 0
+
+		}
+		if (sec <=9) {
+			Seconds.innerText = "0" +sec;
+		}
+		setTimeout("timer()", 1000)
+
+	}
+}
